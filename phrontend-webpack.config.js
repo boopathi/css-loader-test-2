351f77cb120605d34245d8ffa02032faae2bf7bc
Boopathi Rajaa <me@boopathi.in>
2015-08-20 17:22:17 +0530

    Add phrontend-webpack code

---
+++ phrontend-webpack.config.js
@@ -0,0 +1,5 @@
+var makeconf = require('phrontend-webpack');
+
+module.exports = makeconf({
+    src: __dirname
+});
--- webpack.config.js
+++ webpack.config.js
@@ -1,20 +1,26 @@
 module.exports = {
-    entry: './index.js',
-    output: {
-        path: 'public',
-        publicPath: '/public',
-        filename: 'bundle.js'
-    },
-    module: {
-        loaders: [
-            {
-                test: /\.css$/,
-                loader: 'css?sourceMap&modules&localIdentName=[path][name]-[local]-[hash:base64:5]'
-            },
-            {
-                test: /\.jpg$/,
-                loader: 'file'
-            }
-        ]
-    }
+  entry: './index.js',
+  output: {
+    path: 'public',
+    publicPath: '/public',
+    filename: 'bundle.js'
+  },
+  postcss: [
+    require('cssnext')(),
+    require('postcss-simple-vars'),
+    require('postcss-mixins'),
+    require('postcss-nested')
+  ],
+  module: {
+    loaders: [
+      {
+        test: /\.css$/,
+        loader: 'css?sourceMap&modules&localIdentName=[path][name]-[local]-[hash:base64:5]!postcss'
+      },
+      {
+        test: /\.jpg$/,
+        loader: 'file'
+      }
+    ]
+  }
 };
